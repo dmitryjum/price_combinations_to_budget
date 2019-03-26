1e224f3108ab0d64769f59916e7fde23b96fa293
Dmitry Jum <xfiler@ymail.com>
2019-03-25 23:56:32 -0400

    tries to find first and smallest set of summands beginning from biggest number, no combination this way, need to reverse and try again

--- combinations.rb
+++ combinations.rb
@@ -1,15 +1,26 @@
 require 'pry'
 
 class Combinations
-  attr_reader :budget, :prices
+  attr_reader :budget, :prices, :least_summands
   def initialize(menu_path)
     parsed_menu = parse_menu(menu_path)
     @budget = parsed_menu[""]
-    @prices = parsed_menu.values.drop(1)
+    @prices = parsed_menu.values.drop(1).sort.reverse
+    @least_summands = []
   end
 
-  def get_all_sums
-    binding.pry
+  def get_all_sums(prices)
+    current_sum = @least_summands.reduce(:+).to_f
+    return @least_summands if prices.empty?
+    if current_sum < budget
+      @least_summands << prices.first
+      get_all_sums(prices)
+    elsif current_sum > budget
+      @least_summands.pop
+      get_all_sums(prices.drop(1))
+    else
+      @least_summands
+    end
   end
 
   private
@@ -24,4 +35,5 @@ class Combinations
 end
 
 combs = Combinations.new('./menu.txt')
-binding.pry
+combs.get_all_sums(combs.prices)
+puts combs.least_summands
